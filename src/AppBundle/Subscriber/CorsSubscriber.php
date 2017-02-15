5481218bf07badec382697cc3badbc4cc86a1325
Natanael Oliva <natanael.og@gmail.com>
2017-02-15 11:41:20 +0000

    added options header

--- src/AppBundle/Subscriber/CorsSubscriber.php
+++ src/AppBundle/Subscriber/CorsSubscriber.php
@@ -14,7 +14,6 @@ class CorsSubscriber
 
         $response = $event->getResponse();
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT');
-        $response->headers->set('Access-Control-Allow-Headers', 'X-Header-One,X-Header-Two');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS');
     }
 }
